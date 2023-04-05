be074ef08499cb886353f7aaa5115d05c6515a59
Kauan014 <129955630+Kauan014@users.noreply.github.com>
2023-04-05 13:40:25 -0300

    alteraçao js

--- main.js
+++ main.js
@@ -16,7 +16,7 @@ document.getElementById("btn-soma").addEventListener( "click",calculo);
 
 function calculo()
 {
-    let num1= document.getElementById("num1").value;
-    let num2=document.getElementById("num2").value;
+    let num1 = document.getElementById("num1").value;
+    let num2 = document.getElementById("num2").value;
     document.getElementById("resultado1").innerHTML= perseFloat(num1) + perseFloat(num2);
 }
